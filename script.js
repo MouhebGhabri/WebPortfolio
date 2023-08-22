f40a754268ab6394012aeb1fff6a6d235a9e614a
MouhebGhabri <ghabrimouheb@gmail.com>
2023-08-22 17:23:44 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+  strings:["Front-end Developer","Back-end Developer","Mobile Developer"],
+  typeSpeed:100,
+  // backSpeed:100,
+  // backDelay:25,
+   loop:true
+});
